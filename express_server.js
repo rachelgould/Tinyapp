758130222de62eb1545fcee368d8b8125fd210cb
Rachel Gould <rachegould@gmail.com>
2019-02-13 17:46:06 +0000

    Minor refactoring

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@ function generateRandomString() {
   let randomString = "";
   const allowableCharacters = ('abcdefghijklmnopqrstuvwxyz' + 'abcdefghijklmnopqrstuvwxyz'.toUpperCase() + '1234567890').split('');
   for (let i = 0; i < 6; i++) {
+    // Retrieve a random character from the 62-element array of allowable characters
     let index = Math.floor(Math.random() * 62);
     randomString += allowableCharacters[index];
   }
@@ -20,12 +21,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 // This needs to be above the route for /urls/:shortURL because it takes precedence, and otherwise Express will think new is a route parameter
@@ -39,14 +41,16 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase }; //This HAS to be in the form of an object, even if there's only 1 key
-  res.render("urls_index", templateVars); //urls_index is the .ejs file that's being passed the templateVars object. EJS automatically looks in a views folder, and appends the .ejs extension to urls_index
+  let templateVars = { urls: urlDatabase }; // This HAS to be in the form of an object, even if there's only 1 key
+  res.render("urls_index", templateVars); // urls_index is the .ejs file that's being passed the templateVars object. EJS automatically looks in a views folder, and appends the .ejs extension to urls_index
 });
+
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  urlDatabase[generateRandomString()] = [req.body.longURL]; // Adds it to our url database
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = [req.body.longURL]; // Adds it to our url database
+  res.redirect(`/urls/${newShortURL}`); // Redirect to the page for the newly-generated short URL
 });
 
 app.listen(PORT, () => {
